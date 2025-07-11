f6b34bd043694f878137451c0132b597a93e555b
Ayowole Sodipe <haywhy2946@gmail.com>
2025-07-11 13:33:06 +0100

    splash, onboarding and login UI done

--- lib/features/auth/presentation/pages/login_screen.dart
+++ lib/features/auth/presentation/pages/login_screen.dart
@@ -3,234 +3,259 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:grocery_mart/core/theme/colors.dart';
 
-class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+class LoginScreen extends StatefulWidget {
+  final TextEditingController emailController;
+  final TextEditingController passwordController;
+
+  const LoginScreen({
+    super.key,
+    required this.emailController,
+    required this.passwordController,
+  });
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool _isPasswordVisible = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFFFEFEFE),
+      backgroundColor: Color(0xffffefefe),
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        child: Stack(
           children: [
-            Stack(
-              children: [
-                Container(
-                  width: double.infinity,
-                  height: 300.h,
-                  decoration: const BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage('assets/upper_blur.png'),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
+            Container(
+              width: double.infinity,
+              height: 300.h,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/upper_blur.png'),
+                  fit: BoxFit.cover,
                 ),
-
-                Positioned.fill(
-                  child: Align(
-                    alignment: Alignment.center,
-                    child: Image.asset(
-                      'assets/coloured_carrot.png',
-                      height: 48.h,
-                      width: 56.w,
-                    ),
-                  ),
-                ),
-              ],
+              ),
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 24.w),
+            Positioned(
+              top: 0,
+              left: 0,
+              right: 0,
+              height: 300.h,
+              child: Align(
+                alignment: Alignment.center,
+                child: Image.asset(
+                  'assets/coloured_carrot.png',
+                  height: 48.h,
+                  width: 56.w,
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                width: double.infinity,
+                height: 300.h,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/lower_blur.png'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+            SingleChildScrollView(
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                    "Log In",
-                    style: GoogleFonts.roboto(
-                      fontSize: 26.sp,
-                      fontWeight: FontWeight.w600,
-                      color: AppColors.black,
-                      letterSpacing: 0.sp,
-                      height: 1.1, // = 29 / 26
-                    ),
-                  ),
-
-                  SizedBox(height: 15.h),
-
-                  Text(
-                    'Enter your email and password',
-                    style: GoogleFonts.roboto(
-                      fontSize: 16.sp,
-                      fontWeight: FontWeight.w400,
-                      color: Color(0xFF7c7c7c),
-                      letterSpacing: 0.sp,
-                      height: 0.9, // = 15 / 16
-                    ),
-                  ),
-
-                  SizedBox(height: 40.h),
-                  const Text(
-                    'Email',
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Color(0xFF718096),
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-
-                  const SizedBox(height: 12),
-
-                  Container(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 0,
-                      vertical: 8,
-                    ),
-                    decoration: const BoxDecoration(
-                      border: Border(
-                        bottom: BorderSide(color: Color(0xFFE2E8F0), width: 1),
-                      ),
-                    ),
-                    child: const Text(
-                      'imshuvo97@gmail.com',
-                      style: TextStyle(
-                        fontSize: 20,
-                        color: Color(0xFF2D3748),
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                  ),
-
-                  SizedBox(height: 30.h),
-
-                  const Text(
-                    'Password',
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Color(0xFF718096),
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-
-                  const SizedBox(height: 12),
-
-                  // Password Input
-                  Container(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 0,
-                      vertical: 8,
-                    ),
-                    decoration: const BoxDecoration(
-                      border: Border(
-                        bottom: BorderSide(color: Color(0xFFE2E8F0), width: 1),
-                      ),
-                    ),
-                    child: Row(
+                  SizedBox(height: 300.h),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 24.w),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        const Expanded(
-                          child: Text(
-                            '••••••••',
-                            style: TextStyle(
-                              fontSize: 24,
-                              color: Color(0xFF2D3748),
-                              fontWeight: FontWeight.w500,
-                            ),
+                        Text(
+                          "Log In",
+                          style: GoogleFonts.roboto(
+                            fontSize: 26.sp,
+                            fontWeight: FontWeight.w600,
+                            color: AppColors.black,
+                            letterSpacing: 0.sp,
+                            height: 1.1,
                           ),
                         ),
-                        Icon(
-                          Icons.visibility_off,
-                          color: Colors.grey[400],
-                          size: 24,
-                        ),
-                      ],
-                    ),
-                  ),
-
-                  const SizedBox(height: 20),
-
-                  // Forgot Password
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: TextButton(
-                      onPressed: () {},
-                      child: Text(
-                        "Forgot password?",
-                        style: GoogleFonts.roboto(
-                          fontSize: 14.sp,
-                          fontWeight: FontWeight.w400,
-                          color: Color(0xFF181725),
-                          letterSpacing: 0.sp,
-                          height: 1.1, // = 15 / 14
-                        ),
-                      ),
-                    ),
-                  ),
-
-                  const SizedBox(height: 40),
-
-                  Stack(
-                    children: [
-                      Container(
-                        width: double.infinity,
-                        height: 300.h,
-                        decoration: const BoxDecoration(
-                          image: DecorationImage(
-                            image: AssetImage('assets/lower_blur.png'),
-                            fit: BoxFit.cover,
+                        SizedBox(height: 15.h),
+                        Text(
+                          'Enter your email and password',
+                          style: GoogleFonts.roboto(
+                            fontSize: 16.sp,
+                            fontWeight: FontWeight.w400,
+                            color: Color(0xFF7c7c7c),
+                            letterSpacing: 0.sp,
+                            height: 0.9,
                           ),
                         ),
-                      ),
-                      Column(
-                        children: [
-                          Container(
-                            width: double.infinity,
-                            height: 56,
-                            decoration: BoxDecoration(
-                              color: AppColors.primary,
-                              borderRadius: BorderRadius.circular(16),
+                        SizedBox(height: 40.h),
+
+                        TextField(
+                          controller: widget.emailController,
+                          style: GoogleFonts.roboto(
+                            fontSize: 18.sp,
+                            fontWeight: FontWeight.w400,
+                            color: AppColors.black,
+                            height: 1.1,
+                          ),
+                          decoration: InputDecoration(
+                            labelText: 'Email',
+                            labelStyle: GoogleFonts.roboto(
+                              fontSize: 16.sp,
+                              fontWeight: FontWeight.w600,
+                              color: const Color(0xFF7c7c7c),
+                              height: 1.3,
                             ),
-                            child: ElevatedButton(
-                              onPressed: () {},
-                              style: ElevatedButton.styleFrom(
-                                backgroundColor: Colors.transparent,
-                                shadowColor: Colors.transparent,
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(28),
-                                ),
+
+                            enabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(
+                                color: const Color(0xFF7c7c7c),
+                                width: 1.w,
                               ),
-                              child: const Text(
-                                'Log In',
-                                style: TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.w600,
-                                  color: Colors.white,
-                                ),
+                            ),
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(
+                                color: AppColors.primary,
+                                width: 2.w,
                               ),
                             ),
                           ),
-                          const SizedBox(height: 30),
+                          keyboardType: TextInputType.emailAddress,
+                        ),
+                        SizedBox(height: 20.h),
+                        TextField(
+                          controller: widget.passwordController,
+                          style: GoogleFonts.roboto(
+                            fontSize: 18.sp,
+                            fontWeight: FontWeight.w400,
+                            color: AppColors.black,
+                            height: 1.1,
+                          ),
 
-                          RichText(
-                            text: const TextSpan(
+                          decoration: InputDecoration(
+                            labelText: 'Password',
+                            labelStyle: GoogleFonts.roboto(
+                              fontSize: 16.sp,
+                              fontWeight: FontWeight.w600,
+                              color: const Color(0xFF7c7c7c),
+                              height: 1.3,
+                            ),
+                            enabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(
+                                color: const Color(0xFF7c7c7c),
+                                width: 1.w,
+                              ),
+                            ),
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(
+                                color: AppColors.primary,
+                                width: 2.w,
+                              ),
+                            ),
+                            suffixIcon: IconButton(
+                              icon: Icon(
+                                _isPasswordVisible
+                                    ? Icons.visibility
+                                    : Icons.visibility_off,
+                                color: const Color(0xFF7c7c7c),
+                                size: 20.sp,
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  _isPasswordVisible = !_isPasswordVisible;
+                                });
+                              },
+                            ),
+                          ),
+                          obscureText: !_isPasswordVisible, // Toggle visibility
+                        ),
+                        SizedBox(height: 20.h),
+                        Align(
+                          alignment: Alignment.centerRight,
+                          child: TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              "Forgot password?",
+                              style: GoogleFonts.roboto(
+                                fontSize: 14.sp,
+                                fontWeight: FontWeight.w400,
+                                color: Color(0xFF181725),
+                                letterSpacing: 0.sp,
+                                height: 1.1,
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 30.h),
+                        Container(
+                          width: double.infinity,
+                          height: 56.h,
+                          decoration: BoxDecoration(
+                            color: AppColors.primary,
+                            borderRadius: BorderRadius.circular(16.r),
+                          ),
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.transparent,
+                              shadowColor: Colors.transparent,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(28.r),
+                              ),
+                            ),
+                            child: Text(
+                              'Log In',
+                              style: GoogleFonts.roboto(
+                                fontSize: 18.sp,
+                                fontWeight: FontWeight.w600,
+                                color: AppColors.white,
+                                letterSpacing: 0.sp,
+                                height: 1, // = 18 / 18
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 30.h),
+                        Align(
+                          alignment: Alignment.center,
+                          child: RichText(
+                            text: TextSpan(
                               text: "Don't have an account? ",
-                              style: TextStyle(
-                                fontSize: 16,
-                                color: Color(0xFF2D3748),
-                                fontWeight: FontWeight.w500,
+                              style: GoogleFonts.roboto(
+                                fontSize: 14.sp,
+                                fontWeight: FontWeight.w600,
+                                color: AppColors.black,
+                                letterSpacing: 0.sp,
+                                height: 1, // = 29 / 48
                               ),
+
                               children: [
                                 TextSpan(
-                                  text: 'Singup',
-                                  style: TextStyle(
-                                    color: Color(0xFF48BB78),
+                                  text: 'Sign Up',
+                                  style: GoogleFonts.roboto(
+                                    fontSize: 14.sp,
                                     fontWeight: FontWeight.w600,
+                                    color: AppColors.primary,
+                                    letterSpacing: 0.sp,
+                                    height: 1, // = 29 / 48
                                   ),
                                 ),
                               ],
                             ),
                           ),
-                        ],
-                      ),
-                    ],
+                        ),
+                        SizedBox(height: 300.h),
+                      ],
+                    ),
                   ),
                 ],
               ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,34 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter_screenutil/flutter_screenutil.dart';
+// import 'package:grocery_mart/core/shared/onboarding_screen.dart';
+// import 'package:grocery_mart/core/shared/splash_screen.dart';
+// import 'package:grocery_mart/features/auth/presentation/pages/login_screen.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return ScreenUtilInit(
+//       designSize: const Size(414, 896),
+//       minTextAdapt: true,
+//       splitScreenMode: true,
+//       builder: (context, child) {
+//         return MaterialApp(
+//           debugShowCheckedModeBanner: false,
+//           // home: const OnboardingScreen(),
+//           // home: const NectarSplashScreen(),
+//           home: const LoginScreen(),
+//         );
+//       },
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:grocery_mart/core/shared/onboarding_screen.dart';
@@ -5,11 +36,13 @@ import 'package:grocery_mart/core/shared/splash_screen.dart';
 import 'package:grocery_mart/features/auth/presentation/pages/login_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +55,14 @@ class MyApp extends StatelessWidget {
           debugShowCheckedModeBanner: false,
           // home: const OnboardingScreen(),
           // home: const NectarSplashScreen(),
-          home: const LoginScreen(),
+          home: LoginScreen(
+            emailController: emailController,
+            passwordController: passwordController,
+          ),
+          // home: LogScreen(
+          //   emailController: emailController,
+          //   passwordController: passwordController,
+          // ),
         );
       },
     );
